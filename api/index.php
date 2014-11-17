4dc635271de391bfae3bdb245f6f70442208fd65
kublahanov <4progs@inbox.ru>
2014-11-18 00:13:59 +0300

    Авто-добавление проектов: тест 1

--- api/index.php
+++ api/index.php
@@ -4,51 +4,108 @@
  */
 if (count($_GET) < 1)
 {
-    die();
+    // die();
 }
 
-/*
- * Сканиреум корневую директорию на наличие проектов.
- * Выбираем только папки с .ru и .net
+/**
+ * Класс с полями для БД pr_list
  */
-if ($_GET['projects'] && $_GET['projects'] == 'all')
+class myProject
 {
+    public $sName, $sDesc, $sURL, $sType;
+
+    /**
+     * [__construct description]
+     * @param [type] $sName [description]
+     * @param [type] $sDesc [description]
+     * @param [type] $sURL  [description]
+     * @param [type] $sType [description]
+     */
+    public function __construct($sName, $sDesc, $sURL, $sType)
+    {
+        $this->sName = $sName;
+        $this->sDesc = $sDesc;
+        $this->sURL = $sURL;
+        $this->sType = $sType;
+    }
+
+    /**
+     * [getArray description]
+     * @return [type] [description]
+     */
+    public static function getArray()
+    {
+        return array(
+            'name' => $this->sName,
+            'desc' => $this->sDesc,
+            'url' => $this->sURL,
+            'type' => $this->sType
+        );
+    }
+}
+
+try
+{
+    /*
+     * Получаем информацию о проектах сохранённую в БД
+     */
+    $dbFile = dirname(__FILE__) . '/projects.db';
+    $dbLink = new PDO('sqlite:' . $dbFile);
+    $dbLink->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sQuery = "SELECT * FROM pr_list";
+    $dbStat = $dbLink->query($sQuery);
+    $dbStat->setFetchMode(PDO::FETCH_ASSOC);
+
+    $arDBData = array();
+    while ($arRow = $dbStat->fetch())
+    {
+        $arDBData[$arRow['name']] = $arRow;
+    }
+
+    /*
+     * Получаем информацию о проектах из ФС.
+     * Сканируем корневую директорию на наличие проектов.
+     * Выбираем только папки с .ru и .net.
+     */
     $sHomeDir = '/var/www/';
-    $arDirData = scandir($sHomeDir);
-    foreach ($arDirData as $iKey => $sDir)
+    $arFSData = scandir($sHomeDir);
+    foreach ($arFSData as $iKey => $sDir)
     {
         if ((strpos($sDir, '.ru') === false) && (strpos($sDir, '.net') === false))
         {
-            unset($arDirData[$iKey]);
+            unset($arFSData[$iKey]);
+            continue;
         }
+        $arFSData[$iKey] = ((check2LvlDomain($sDir)) ? 'www.' : '') . $sDir;
     }
-}
 
-if ($_GET['create_db'] && $_GET['create_db'] == 'sqlite')
-{
-    try
+    /*
+     * Сравниваем данные из БД и из ФС
+     */
+    foreach ($arFSData as $sDir)
     {
-        $dbFile = dirname(__FILE__) . '/projects.db';
-        $dbLink = new PDO('sqlite:' . $dbFile);
-        $dbLink->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        // $dbLink->prepare('DELECT name FROM people')->execute();
-
-        foreach ($arDirData as $sDir)
+        if (array_key_exists($sDir, $arDBData))
         {
-            $sDir = ((check2LvlDomain($sDir)) ? 'www.' : '') . $sDir;
-            $sQuery = "INSERT INTO pr_list (`name`, `desc`, `url`, `type`) values ('$sDir', '$sDir', 'http://$sDir/', 1)";
-            $dbStat = $dbLink->prepare($sQuery);  
-            $dbStat->execute();
+            echo "- найдено: $sDir<br>";
         }
-
-        $dbLink = null;
-    }  
-    catch (PDOException $e)
-    {
-        file_put_contents('PDOErrors.log', $e->getMessage(), FILE_APPEND);
-        echo $e->getMessage();
+        else
+        {
+            echo "- не найдено: $sDir<br>";
+            echo " Добавляем!<br>";
+            // $PROJECT = new myProject($sDir, $sDir, 'http://' . $sDir . '/', 0);
+            // $sQuery = "INSERT INTO pr_list (`name`, `desc`, `url`, `type`) values (:name, :desc, :url, :type)";
+            // $dbStat = $dbLink->prepare($sQuery);
+            // $dbStat->execute($PROJECT::getArray());
+        }
     }
+
+    $dbLink = null;
+}
+catch (PDOException $e)
+{
+    file_put_contents('PDOErrors.log', $e->getMessage(), FILE_APPEND);
+    echo $e->getMessage();
 }
 
 ?>
@@ -76,7 +133,7 @@ if ($_GET['create_db'] && $_GET['create_db'] == 'sqlite')
     <div class="container">
         <h1>Все проекты</h1>
         <ul class="all-projects">
-        <?foreach ($arDirData as $sDir):?>
+        <?foreach ($arFSData as $sDir):?>
             <li>
                 <? $sDir = ((check2LvlDomain($sDir)) ? 'www.' : '') . $sDir; ?>
                 <?=$sDir?>
@@ -89,6 +146,11 @@ if ($_GET['create_db'] && $_GET['create_db'] == 'sqlite')
 </html>
 <?
 
+echo '<pre>';
+print_r($arDBData);
+print_r($arFSData);
+echo '</pre>';
+
 /*
  * Функция возвращает true, если ей передан домен 2-го уровня
  */
@@ -98,9 +160,14 @@ function check2LvlDomain($sDomainName)
     return (count($arSplit) < 3);
 }
 
-// echo '<pre>';
-// print_r($arDirData);
-// echo '</pre>';
+// $arData = array(
+//     'name' => $sDir,
+//     'desc' => $sDir,
+//     'url' => 'http://' . $sDir . '/',
+//     'type' => 0
+// );
+
+// $dbLink->prepare('DELECT name FROM people')->execute();
 
 // $sqlFile = dirname(__FILE__) . '/schema.sqlite.sql';
 // @unlink($dbFile);
@@ -116,3 +183,4 @@ function check2LvlDomain($sDomainName)
 //         $db->exec($sql);
 //     }
 // }
+// http://yii.achievment.ru/pladm/phpliteadmin.php?action=row_view&table=tbl_tag
